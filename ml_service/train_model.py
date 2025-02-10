4fd8145c862ccee998621ce35bb4aa88d80bd58f
girgil <giraud.nicolas36@gmail.com>
2025-02-10 10:28:05 +0100

    Modifications db mongo

--- ml_service/train_model.py
+++ ml_service/train_model.py
@@ -4,15 +4,14 @@ from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 import pickle
 
 mongo_client = MongoClient("mongodb://mongo:27017/")
-db = mongo_client["github"]
-collection = db["repos"]
+db = mongo_client["repertoires_db"]
+rep_features = db["repertoires_features"]
+rep_vecs = db["repertoires_vecs"]
 
-repos = list(collection.find({}, {"name": 1, "description": 1}))
-documents = [TaggedDocument(words=(repo["description"] or "").split(), tags=[repo["name"]]) for repo in repos]
+repos_features = list(rep_features.find({}, {"id": 1, "readme_preproc": 1, "others_preproc": 1, "url": 1}))
+documents = [TaggedDocument(words=(repos_features["readme_preproc"] or "").split(), tags=[repo["readme_preproc"]]) for repo in repos_features]
 
 model = Doc2Vec(documents, vector_size=50, window=2, min_count=1, workers=4)
-
-import pickle
 
 # save
 with open('/data/doc2vec.pk','wb') as f:
--- mongo/init.js
+++ mongo/init.js
@@ -2,16 +2,20 @@
 db = db.getSiblingDB("github");
 
 // Création de la collection des dépôts GitHub
-db.createCollection("repos");
+db.createCollection("repertoires_vecs");
+db.createCollection("repertoires_features");
+db.createCollection("users");
 
 // Création d’un index sur le champ "name" pour accélérer les recherches
-db.repos.createIndex({ name: 1 }, { unique: true });
+// db.repos.createIndex({ name: 1 }, { unique: true });
+db.users.createIndex({ id : 1 }, { unique: true });
+db.users.insert({ id: -1 }) // exemple user
 
-// Ajout de quelques dépôts de test (optionnel)
-db.repos.insertMany([
-    { name: "tensorflow", description: "An open source machine learning framework" },
-    { name: "scikit-learn", description: "Machine learning in Python" },
-    { name: "pytorch", description: "An open source deep learning platform" }
-]);
+db.repertoires_vecs.createIndex({ id: 1 }, { unique: true });
+db.repertoires_vecs.insert({ id: -1 }); // exemple repertoire vec
 
-print("✅ Base de données et index MongoDB initialisés !");
+db.repertoires_features.createIndex({ id: 1 }, { unique: true });
+db.repertoires_features.insert({ id: -1, readme_preproc: "['test', 'mot']", others_preproc: "['design', 'python']", url: "https://api.github.com/repos/seek-oss/playroom" })
+
+
+print("✅ Bases de données et index MongoDB initialisés !");
